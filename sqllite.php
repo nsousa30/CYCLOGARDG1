72bd135951905ea61f7422e0c36fa578bace4684
nsousa30 <126089507+nsousa30@users.noreply.github.com>
2023-06-15 15:09:41 +0100

    Update sqllite.php

--- sqllite.php
+++ sqllite.php
@@ -1,11 +1,11 @@
 <?php
-// Obter os dados enviados pela aplicação Android
+
 $id = $_POST['ID'];
 $data = $_POST['Data'];
 $latitude = $_POST['Latitude'];
 $longitude = $_POST['Longitude'];
 
-// Conectar-se à base de dados MySQL
+
 $servername = "localhost";
 $username = "CYCLOGARD";
 $password = "1234";
@@ -13,12 +13,12 @@ $dbname = "cyclogard";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar a conexão
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Inserir os dados na base de dados MySQL
+
 $sql = "INSERT INTO cyclogard (ID, Data, Latitude, Longitude) VALUES ('$id', '$data', '$latitude', '$longitude')";
 if ($conn->query($sql) === TRUE) {
     echo "Data inserted successfully";
@@ -26,7 +26,7 @@ if ($conn->query($sql) === TRUE) {
     echo "Error inserting data: " . $conn->error;
 }
 
-// Fechar a conexão com o banco de dados
+
 $conn->close();
 ?>
 
